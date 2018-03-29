97b3dd93d5a247374490fba6c0780cf3b156625f
ZqhKHP <G00336837@gmit.ie>
2018-03-29 13:39:54 +0100

    create init components method to draw the frame

--- MessengerClient/src/ie/gmit/sw/MessageChatYurt.java
+++ MessengerClient/src/ie/gmit/sw/MessageChatYurt.java
@@ -66,12 +66,20 @@ public class MessageChatYurt extends JFrame {
 	 * Create the frame.
 	 */
 	public MessageChatYurt() {
+		
+		//initialize components
+		initComponents();
+		
+	}
+
+	public void initComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
 	}
 
 }
