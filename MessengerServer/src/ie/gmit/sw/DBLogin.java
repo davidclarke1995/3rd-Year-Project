a178713c26139618edbe9272b0890d89d7d3dea2
ZqhKHP <G00336837@gmit.ie>
2018-03-29 19:40:14 +0100

    adding neccessary imports

--- MessengerServer/src/ie/gmit/sw/DBLogin.java
+++ MessengerServer/src/ie/gmit/sw/DBLogin.java
@@ -1,5 +1,23 @@
 package ie.gmit.sw;
 
+import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.net.*;
+
+
 public class DBLogin {
 
 }
