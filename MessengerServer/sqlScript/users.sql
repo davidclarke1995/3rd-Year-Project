a5efec0b63d27df4226491543d629147409efed6
Eoghan-oconnor <G00330972@gmit.ie>
2018-04-03 00:33:15 +0100

    Adding .sql file for default users

---
+++ MessengerServer/sqlScript/users.sql
@@ -0,0 +1,5 @@
+--creating default users 
+INSERT INTO userDetails (userName, name, password) VALUES ('Harry12', 'Harry Keane', 'password'),
+('Dylannnn', 'Dylan Drake','password556'),
+('Marky', 'Mark McGrath','password');
+
