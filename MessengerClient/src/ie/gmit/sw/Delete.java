47c2b320ce96fa90b1698fb6d7dc6cc91951dc2f
ZqhKHP <G00336837@gmit.ie>
2018-04-06 00:28:17 +0100

    adding text fields and buttons and labels to delete page

--- MessengerClient/src/ie/gmit/sw/Delete.java
+++ MessengerClient/src/ie/gmit/sw/Delete.java
@@ -6,10 +6,18 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class Delete extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -47,8 +55,55 @@ public class Delete extends JFrame {
 			setBounds(100, 100, 450, 300);
 			contentPane = new JPanel();
 			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-			contentPane.setLayout(new BorderLayout(0, 0));
 			setContentPane(contentPane);
+			
+			textField = new JTextField();
+			textField.setColumns(10);
+			
+			passwordField = new JPasswordField();
+			
+			JLabel lblUsername = new JLabel("Username:");
+			
+			JLabel lblPassword = new JLabel("Password");
+			
+			JLabel lblEnterYourDetails = new JLabel("Enter your details below:");
+			
+			JButton btnDelete = new JButton("Delete");
+			GroupLayout gl_contentPane = new GroupLayout(contentPane);
+			gl_contentPane.setHorizontalGroup(
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+						.addContainerGap(116, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+							.addComponent(lblUsername)
+							.addComponent(lblPassword))
+						.addGap(40)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+							.addComponent(btnDelete)
+							.addComponent(lblEnterYourDetails)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(passwordField, Alignment.LEADING)
+								.addComponent(textField, Alignment.LEADING)))
+						.addGap(136))
+			);
+			gl_contentPane.setVerticalGroup(
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
+					.addGroup(gl_contentPane.createSequentialGroup()
+						.addContainerGap()
+						.addComponent(lblEnterYourDetails)
+						.addGap(38)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+							.addComponent(lblUsername)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addGap(18)
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+							.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addComponent(lblPassword))
+						.addGap(18)
+						.addComponent(btnDelete)
+						.addContainerGap(89, Short.MAX_VALUE))
+			);
+			contentPane.setLayout(gl_contentPane);
 		}
 		
 
