ad18dd63abf1b42a1bab592904ec24da0601411e
Eoghan-oconnor <G00330972@gmit.ie>
2018-04-05 21:37:16 +0100

    Adding db interface

--- MessengerClient/src/ie/gmit/sw/NewLogin.java
+++ MessengerClient/src/ie/gmit/sw/NewLogin.java
@@ -39,9 +39,9 @@ public class NewLogin extends JFrame {
 	private JTextField textField;
 	private JPasswordField passwordField;
 	private JTextArea textArea;
-	
-	//private JPanel contentPane;
-    Socket requestSocket;
+
+	// private JPanel contentPane;
+	Socket requestSocket;
 	ObjectOutputStream out;
 	ObjectInputStream in;
 	String message = "x";
@@ -68,12 +68,12 @@ public class NewLogin extends JFrame {
 	 * Create the frame.
 	 */
 	public NewLogin() {
-		
+
 		initComponents();
-		//connect to the server at local host
-	    acceptConnections();
-		//createEvents();
-		
+		// connect to the server at local host
+		acceptConnections();
+		// createEvents();
+
 	}
 
 	public void initComponents() {
@@ -83,31 +83,30 @@ public class NewLogin extends JFrame {
 		contentPaneMain.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPaneMain.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPaneMain);
-		
-		//functionality 
-		
+
+		// functionality
+
 		JLabel lblUsername = new JLabel("UserName:");
-		
+
 		textField = new JTextField();
 		textField.setColumns(10);
-		
+
 		JLabel lblPassword = new JLabel("Password:");
-		
+
 		passwordField = new JPasswordField();
 		passwordField.setColumns(10);
-		
+
 		JLabel lblNewLabel = new JLabel();
 		lblNewLabel.setText("Enter your details");
-		
+
 		JButton btnLogin = new JButton("Login");
 		btnLogin.addActionListener(new ActionListener() {
-			//@SuppressWarnings("deprecation")
+			// @SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent arg0) {
 				///////////////////////
 				String userName = textField.getText();
 				String password = passwordField.getText();
-				
-				
+
 				sendMessage("login " + userName + " " + password);
 				try {
 					message = (String) in.readObject();
@@ -118,115 +117,83 @@ public class NewLogin extends JFrame {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				if(message.equals("log")) {
+
+				if (message.equals("log")) {
 					outPutMessage("////////////////////////////////" + userName);
 					MessageChatYurt myChat;
 					myChat = new MessageChatYurt(userName);
 					myChat.setVisible(true);
 				}
-				
-				else{
-					//txtrHello.setText("Invalid username or password");
+
+				else {
+					// txtrHello.setText("Invalid username or password");
 					lblNewLabel.setText("Invalid username or password");
 				}
-				
-					
-				//hard coded user before connecting it to a database
+
+				// hard coded user before connecting it to a database
 				/*
-				if(userName.equals("GaryConnelly") && password.equals("gary") || userName.equals("DaveClarke") && password.equals("dave")){
-					MessageChatYurt myChat;
-					myChat = new MessageChatYurt();
-					myChat.setVisible(true);
-					//myChat.run();
-					
-				}
-				else{
-					//txtrHello.setText("Invalid username or password");
-					lblNewLabel.setText("Invalid username or password");
-				}
-				*/
-				
+				 * if(userName.equals("GaryConnelly") && password.equals("gary")
+				 * || userName.equals("DaveClarke") && password.equals("dave")){
+				 * MessageChatYurt myChat; myChat = new MessageChatYurt();
+				 * myChat.setVisible(true); //myChat.run();
+				 * 
+				 * } else{ //txtrHello.setText("Invalid username or password");
+				 * lblNewLabel.setText("Invalid username or password"); }
+				 */
+
 			}
 
-			
-		});//end action listener
-		
-		
-		
-		
-		
-		
-		
-		GroupLayout gl_contentPaneMain = new GroupLayout(contentPaneMain);
-		gl_contentPaneMain.setHorizontalGroup(
-			gl_contentPaneMain.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPaneMain.createSequentialGroup()
-					.addGap(108)
-					.addGroup(gl_contentPaneMain.createParallelGroup(Alignment.LEADING, false)
-						.addGroup(gl_contentPaneMain.createSequentialGroup()
-							.addComponent(lblPassword)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(passwordField))
-						.addGroup(gl_contentPaneMain.createSequentialGroup()
-							.addComponent(lblUsername)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(173, Short.MAX_VALUE))
-				.addGroup(gl_contentPaneMain.createSequentialGroup()
-					.addGap(82)
-					.addComponent(btnLogin, GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE)
-					.addGap(91))
-				.addGroup(gl_contentPaneMain.createSequentialGroup()
-					.addGap(184)
-					.addComponent(lblNewLabel)
-					.addContainerGap(194, Short.MAX_VALUE))
-		);
-		gl_contentPaneMain.setVerticalGroup(
-			gl_contentPaneMain.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPaneMain.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPaneMain.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblUsername)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(gl_contentPaneMain.createParallelGroup(Alignment.LEADING)
-						.addComponent(lblPassword)
-						.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(18)
-					.addComponent(btnLogin)
-					.addGap(18)
-					.addComponent(lblNewLabel)
-					.addContainerGap(121, Short.MAX_VALUE))
-		);
-		contentPaneMain.setLayout(gl_contentPaneMain);
-		
-		
-		
-		
-		
+		});// end action listener
 
-	}//end init components
-	
-	
-	//accept connections method
+		GroupLayout gl_contentPaneMain = new GroupLayout(contentPaneMain);
+		gl_contentPaneMain.setHorizontalGroup(gl_contentPaneMain.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPaneMain
+						.createSequentialGroup().addGap(108).addGroup(gl_contentPaneMain
+								.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(gl_contentPaneMain.createSequentialGroup().addComponent(lblPassword)
+										.addPreferredGap(ComponentPlacement.RELATED).addComponent(passwordField))
+								.addGroup(gl_contentPaneMain.createSequentialGroup().addComponent(lblUsername)
+										.addPreferredGap(ComponentPlacement.RELATED).addComponent(textField,
+												GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+												GroupLayout.PREFERRED_SIZE)))
+						.addContainerGap(173, Short.MAX_VALUE))
+				.addGroup(gl_contentPaneMain.createSequentialGroup().addGap(82)
+						.addComponent(btnLogin, GroupLayout.DEFAULT_SIZE, 251, Short.MAX_VALUE).addGap(91))
+				.addGroup(gl_contentPaneMain.createSequentialGroup().addGap(184).addComponent(lblNewLabel)
+						.addContainerGap(194, Short.MAX_VALUE)));
+		gl_contentPaneMain.setVerticalGroup(gl_contentPaneMain.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPaneMain.createSequentialGroup().addContainerGap()
+						.addGroup(gl_contentPaneMain.createParallelGroup(Alignment.BASELINE).addComponent(lblUsername)
+								.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE))
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addGroup(gl_contentPaneMain.createParallelGroup(Alignment.LEADING).addComponent(lblPassword)
+								.addComponent(passwordField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
+										GroupLayout.PREFERRED_SIZE))
+						.addGap(18).addComponent(btnLogin).addGap(18).addComponent(lblNewLabel)
+						.addContainerGap(121, Short.MAX_VALUE)));
+		contentPaneMain.setLayout(gl_contentPaneMain);
+
+	}// end init components
+
+	// accept connections method
 	private void acceptConnections() {
 		// 1. creating a socket to connect to the server
 		input = new Scanner(System.in);
 		try {//
-			requestSocket = new Socket("35.189.235.6", 80);
+			requestSocket = new Socket("35.190.197.5", 80);
 			// requestSocket = new Socket("35.205.181.61", 2004);
-			//System.out.println("Connected to localhost in port 2004");
+			// System.out.println("Connected to localhost in port 2004");
 			outPutMessage("Connected to server yurt");
 			// 2. get Input and Output streams
 			out = new ObjectOutputStream(requestSocket.getOutputStream());
 			out.flush();
 			in = new ObjectInputStream(requestSocket.getInputStream());
-			
+
 			// connection successful
 			try {
 				message = (String) in.readObject();
-				//System.out.println("server>" + message);
+				// System.out.println("server>" + message);
 				outPutMessage("server>" + message);
 				message = (String) in.readObject();
 				outPutMessage("server>" + message);
@@ -234,7 +201,7 @@ public class NewLogin extends JFrame {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 			message = "x";
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
@@ -243,14 +210,10 @@ public class NewLogin extends JFrame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	}
-	
-	
-	
 
-	//implement send message method
+	}
+
+	// implement send message method
 	private void sendMessage(String msg) {
 		try {
 			out.writeObject(msg);
@@ -260,20 +223,18 @@ public class NewLogin extends JFrame {
 		} catch (IOException ioException) {
 			ioException.printStackTrace();
 		}
-		
-	}//end send message
-	
-///////////////////////////////////////////////////////////////////////////
-// This method is for testing purposes only
-/////////////////////////////////////////////////
-public void outPutMessage(String message) {
-System.out.println(message);
-}
 
-public void outPutInt(int i) {
-System.out.println(i);
-}
-		
-	}//end JFrame
+	}// end send message
 
+	///////////////////////////////////////////////////////////////////////////
+	// This method is for testing purposes only
+	/////////////////////////////////////////////////
+	public void outPutMessage(String message) {
+		System.out.println(message);
+	}
 
+	public void outPutInt(int i) {
+		System.out.println(i);
+	}
+
+}// end JFrame
