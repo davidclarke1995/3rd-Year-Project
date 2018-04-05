e88906cd952abb4052c85f992c0b01869757683e
Eoghan-oconnor <G00330972@gmit.ie>
2018-04-05 21:43:27 +0100

    Adding db interface

---
+++ DatabaseInterface/DatabaseInferface/src/ie/gmit/sw/User.java
@@ -0,0 +1,37 @@
+package ie.gmit.sw;
+
+public class User {
+	
+	private String name;
+	private String userName;
+	private String password;
+	
+	public User() {
+		super();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", userName=" + userName + ", password=" + password + "]";
+	}
+
+}
