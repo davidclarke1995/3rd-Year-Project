8c652451268b750d0b4beadab67c662fc05242bc
ZqhKHP <G00336837@gmit.ie>
2018-04-25 17:09:23 +0100

    commenting the user thats on the server

--- MessengerServer/src/ie/gmit/sw/User.java
+++ MessengerServer/src/ie/gmit/sw/User.java
@@ -1,37 +1,77 @@
 package ie.gmit.sw;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class User.
+ */
 public class User {
 
+	/** The name. */
 	private String name;
+	
+	/** The user name. */
 	private String userName;
+	
+	/** The password. */
 	private String password;
 	
+	/**
+	 * Instantiates a new user.
+	 */
 	public User() {
 		super();
 	}
-	/*
-	public User(String name, String userName, String password) {
-		super();
-		this.name = name;
-		this.userName = userName;
-		this.password = password;
-	}
-	*/
+	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Gets the user name.
+	 *
+	 * @return the user name
+	 */
 	public String getUserName() {
 		return userName;
 	}
+	
+	/**
+	 * Sets the user name.
+	 *
+	 * @param userName the new user name
+	 */
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+	
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
 	public String getPassword() {
 		return password;
 	}
+	
+	/**
+	 * Sets the password.
+	 *
+	 * @param password the new password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
